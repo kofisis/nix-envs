8d87581a5b706fb728e190c9371b109a74ebe685
Nana Amako <erkekofi@yahoo.com>
2025-01-04 00:29:17 +0100

    adding python code to build

---
+++ python/main/main.py
@@ -0,0 +1,21 @@
+import platform as p
+
+
+class RegionLocatior():
+    def find_me(self):
+
+        my_aarchh = p.machine()
+        my_system = p.uname().system
+        my_node = p.uname().node
+        rel_ver = p.uname().version
+
+        print(f"Hi {my_system}, welcome to my {my_node}. My version is {rel_ver} & my architecture is {my_aarchh}")
+
+
+
+
+if __name__ == "__main__":
+    try:
+        RegionLocatior.find_me()
+    except Exception as local_error:
+        print(f"An Error Occured : {local_error}")
