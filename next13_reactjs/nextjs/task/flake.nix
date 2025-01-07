9f2b1d28b8f7f3d22ee8bcbc0eca3050f4dd2428
Nana Amako <erkekofi@yahoo.com>
2025-01-07 01:19:14 +0100

    adding nixFlake for node devShell

---
+++ next13_reactjs/nextjs/task/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix flake to build devShell for node projects";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # install node20
+            nodejs_20
+            # package managers
+            nodePackages.yarn
+            nodePackages.npm
+            # system tools
+            git
+            watchman
+          ];
+          shellHook = '' # Commands to run postShell activation
+            echo "Welcome to nextJS Dev Environment"
+            echo "NodeJS Version: $(node --version)"
+            echo "NPM Version: $(npm --version)"
+            # Run the creation of a nextJS app if package.json not found
+            if [ ! -f package.json ]; then
+              echo "No package.json found. You can create a new project by running:"
+              echo "npx create-next-app@latest"
+            fi
+          '';
+        };
+      }
+    );
+}
